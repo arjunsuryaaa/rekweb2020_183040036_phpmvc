f5c883de9c211016dca7e05a81b9b7e9659ae9b6
arjunsuryaaa <71428301+arjunsuryaaa@users.noreply.github.com>
2020-10-08 11:05:13 +0700

    Upload Folder PHPMVC
    folder latihan PHP MVC

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/profile.png" alt="Arjun Surya Atmaja" width="150" class="rounded-circle shadow">
+	<p>Halo, nama saya <?php echo $data['nama']; ?>, umur saya <?php echo $data['umur']; ?> tahun, saya adalah seorang <?php echo $data['pekerjaan'] ?></p>
+</div>
